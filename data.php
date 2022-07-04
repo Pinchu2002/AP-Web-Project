5a9675b60f2858f12866eceac7c171b26664d5f0
Pinchu2002 <priyanshuchhabda3@gmail.com>
2022-07-05 01:48:01 +0530

    first commit

---
+++ data.php
@@ -0,0 +1,27 @@
+<?php
+$fname = $_POST['fname'];
+$mname = $_POST['mname'];
+$lname = $_POST['lname'];
+$email = $_POST['email'];
+$nationality = $_POST['nationality'];
+$futureteam = $_POST['futureteam'];
+$academy = $_POST['option'];
+$achievements = $_POST['options'];
+$a_details = $_POST['nameyearposition'];
+
+
+$conn = new mysqli('localhost','root','','test');
+if($conn->connect_error){
+    die('Connection Failed: '.$conn->connect_error);
+}
+else{
+    $stmt = $conn->prepare("insert into participants(fname, mname, lname, email, nationality, futureteam,
+    option, options, nameyearposition) values(?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssssssss", $fname, $mname, $lname, $email, $nationality, $futureteam, $option, $options, $nameyearposition);
+    $stmt->execute();
+    echo "Registrated Successfully...";
+    $stmt->close();
+    $conn->close();
+}
+
+?>
